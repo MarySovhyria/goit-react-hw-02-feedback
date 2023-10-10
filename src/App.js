5141268bc27fc73fa8b7bd5752682e2d787471f0
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-10-10 19:14:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Appe from './feedback/feedback.jsx';
+
+function App() {
+  return (
+    <div>
+    <Appe></Appe>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/feedback/feedbackStyled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Good = styled.button `
+    color: green;
+`
+export const Neutral = styled.button `
+    color: grey;
+`
+export const Bad = styled.button `
+    color: red;
+`
