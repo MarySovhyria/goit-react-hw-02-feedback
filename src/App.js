94a77c5be6aaf205250cec50890a09f42ac135a4
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-10-26 22:34:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
 import FeedbackOptions from "./feedback/FeedbackOptions";
 import Statistics from "./feedback/Statistics";
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Notification } from "feedback/Notification";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    // options: ' ',
+const  App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickButtons = (option) => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  onClickButtons = (option) => {
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1
-    }));
-  };
-  countTotal = () => {
-    const {good, neutral, bad} = this.state;
+  const countTotal = () => {
     return good + neutral + bad
   }
-  countPercent = () => {
-    const {good} = this.state;
-    return Math.floor((good / this.countTotal()) * 100);
+  const countPercent = () => {
+    const total = good + neutral + bad
+    return total > 0 ? Math.floor((good / total) * 100) : 0;
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
 
-    const btnNames = Object.keys(this.state)
+    const btnNames = ['good', 'neutral', 'bad']
     return (
       <div className="container">
         <div className="wrapper">
           <FeedbackOptions 
-          onClickButtons={this.onClickButtons}
+          onClickButtons={onClickButtons}
           options={btnNames}
           />
-          {this.countTotal() > 0 ? (
+          {countTotal() > 0 ? (
         <Statistics
           good={good}
           bad={bad}
           neutral={neutral}
-          total={this.countTotal()}
-          percent={this.countPercent()}
+          total={countTotal()}
+          percent={countPercent()}
          
         />
       ) : (
@@ -53,6 +58,5 @@ class App extends Component {
       </div>
     );
   }
-}
 
 export default App;
